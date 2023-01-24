8be42c1ab3121ddd2fbb54e17934b0558b20181d
blancapinot <bpinotj@outlook.com>
2023-01-24 16:05:22 -0300

    Initial commit

---
+++ src/main/java/com/voces/apiresttuvoz/ApirestTuvozApplication.java
@@ -0,0 +1,13 @@
+package com.voces.apiresttuvoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestTuvozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestTuvozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voces/apiresttuvoz/ApirestTuvozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voces.apiresttuvoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestTuvozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
